10524289a8a99a8de5a1563d8a9c8323eadbdac0
Geeroms Davy <dgeeroms@outlook.com>
2022-01-16 12:29:10 +0100

    Clean up boilerplate

--- src/Domain/Dice.cs
+++ src/Domain/Dice.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace Domain
 {
-    internal class Dice
+    public class Dice
     {
+        private Random _randomizer;
+
+        public int Dots { get; set; }
+
+        public Dice()
+        {
+            _randomizer = new Random();
+            Dots = 6;
+        }
+
+        public void Roll()
+        {
+            Dots = _randomizer.Next(1, 7);
+        }
     }
 }
--- src/Domain/Game.cs
+++ src/Domain/Game.cs
@@ -6,7 +6,54 @@ using System.Threading.Tasks;
 
 namespace Domain
 {
-    internal class Game
+    public class Game
     {
+        private IList<int> _highscores;
+        private Dice _dice1;
+        private Dice _dice2;
+        private bool _snakeEyes = false;
+
+        public int Eye1 => _dice1.Dots;
+        public int Eye2 => _dice2.Dots;
+        public bool HasSnakeEyes => _snakeEyes;
+        public IReadOnlyList<int> Highscores => (IReadOnlyList<int>) _highscores;
+        public int Total { get; set; }
+
+        public Game()
+        {
+            _highscores = new List<int>();
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            _dice1 = new Dice();
+            _dice2 = new Dice();
+            Total = 0;
+        }
+
+        public void Play()
+        {
+            _dice1.Roll();
+            _dice2.Roll();
+
+            if(_dice1.Dots == 1 && _dice2.Dots == 1)
+            {
+                _snakeEyes = true;
+                _highscores.Add(Total);
+                Total = 0;
+            } else
+            {
+                Total += _dice1.Dots + _dice2.Dots;
+            }
+
+        }
+
+        public void Restart()
+        {
+            Initialize();
+        }
+
+
     }
 }
